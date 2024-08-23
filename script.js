ba41cfba092e1e25447513fa0533bf719ce5325d
Eduardo Michel Sobutka <158076906+EduardoSobutka@users.noreply.github.com>
2024-08-23 16:33:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const iconeBarrinhas = document.querySelector("#iconeBarrinhas");
+const listaBarrinhas = document.querySelector("div.barrinhas");
+function mostrarLista() {
+  listaBarrinhas.style.display = "block";
+}
